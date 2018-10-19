cce777fb610e04e05853bf6afd31c0a1dd71c792
yuankangbo <yuankangbo@xiaomi.com>
2018-10-19 09:23:36 +0800

    add ForTestApplication

---
+++ app/src/main/java/ForTestApplication.java
@@ -0,0 +1,13 @@
+import android.app.Application;
+
+/**
+ * Created by ykb on 18-10-19.
+ */
+
+public class ForTestApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
