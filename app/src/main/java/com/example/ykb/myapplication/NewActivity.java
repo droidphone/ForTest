0c6fb7f18a667d1384c542525684f6c56a3c9fcb
yuankangbo <yuankangbo@xiaomi.com>
2018-10-19 09:27:09 +0800

    1111111

---
+++ app/src/main/java/com/example/ykb/myapplication/NewActivity.java
@@ -0,0 +1,17 @@
+package com.example.ykb.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ykb on 18-10-19.
+ */
+
+public class NewActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
